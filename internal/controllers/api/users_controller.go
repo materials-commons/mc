fda20eb56429834c25f122dbd6416d647792ca44
gtarcea <glenn.tarcea@gmail.com>
2018-09-10 12:20:57 -0400

    Add additional user store methods and implement controller functions with them

--- internal/controllers/api/users_controller.go
+++ internal/controllers/api/users_controller.go
@@ -58,3 +58,34 @@ func (u *UsersController) GetUserByAPIKey(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, user)
 }
+
+// Login verifies the users password and returns the APIKey
+func (u *UsersController) Login(c echo.Context) error {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.Bind(&req); err != nil {
+		return err
+	}
+
+	user, err := u.UsersStore.GetAndVerifyUser(req.Email, req.Password)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, user)
+}
+
+func (u *UsersController) ResetAPIKey(c echo.Context) error {
+	return nil
+}
+
+func (u *UsersController) ResetPassword(c echo.Context) error {
+	return nil
+}
+
+func (u *UsersController) ModifyUser(c echo.Context) error {
+	return nil
+}
--- internal/store/store_engine.go
+++ internal/store/store_engine.go
@@ -4,4 +4,8 @@ type UsersStoreEngine interface {
 	AddUser(user UserSchema) (UserSchema, error)
 	GetUserByID(id string) (UserSchema, error)
 	GetUserByAPIKey(apikey string) (UserSchema, error)
+	GetAndVerifyUser(id, password string) (UserSchema, error)
+	ModifyUser(id, fullname string) (UserSchema, error)
+	ModifyUserPassword(id, password string) (UserSchema, error)
+	ResetAPIKey(id string) (UserSchema, error)
 }
