b643a0ac7d3248594e4f1ccd2c64e6326eeb94f6
gtarcea <glenn.tarcea@gmail.com>
2018-09-10 13:15:12 -0400

    Modify user store engine interface

--- internal/store/store_engine.go
+++ internal/store/store_engine.go
@@ -4,8 +4,7 @@ type UsersStoreEngine interface {
 	AddUser(user UserSchema) (UserSchema, error)
 	GetUserByID(id string) (UserSchema, error)
 	GetUserByAPIKey(apikey string) (UserSchema, error)
-	GetAndVerifyUser(id, password string) (UserSchema, error)
-	ModifyUser(id, fullname string) (UserSchema, error)
+	ModifyUserFullname(id, fullname string) (UserSchema, error)
 	ModifyUserPassword(id, password string) (UserSchema, error)
-	ResetAPIKey(id string) (UserSchema, error)
+	ModifyUserAPIKey(id, apikey string) (UserSchema, error)
 }
