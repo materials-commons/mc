5dfe6ce5fb66666fdd8833a606841ec52aa06288
gtarcea <glenn.tarcea@gmail.com>
2018-09-10 17:06:52 -0400

    Add outline methods to call to test UserStore

---
+++ internal/store/users_store_test.go
@@ -0,0 +1,31 @@
+package store_test
+
+import "testing"
+
+func TestUsersStore_AddUser(t *testing.T) {
+
+}
+
+func TestUsersStore_GetUserByID(t *testing.T) {
+
+}
+
+func TestUsersStore_GetUserByAPIKey(t *testing.T) {
+
+}
+
+func TestUsersStore_GetAndVerifyUser(t *testing.T) {
+
+}
+
+func TestUsersStore_ModifyUserFullname(t *testing.T) {
+
+}
+
+func TestUsersStore_ModifyUserPassword(t *testing.T) {
+
+}
+
+func TestUsersStore_ModifyUserAPIKey(t *testing.T) {
+
+}
