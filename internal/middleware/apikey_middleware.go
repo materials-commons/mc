9088c32f3b33846988d491496938f24c9223dde0
gtarcea <glenn.tarcea@gmail.com>
2018-09-10 12:19:10 -0400

    Add middleware to authenticate calls by their apikey

---
+++ internal/middleware/apikey_middleware.go
@@ -0,0 +1,72 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/materials-commons/mc/internal/store"
+)
+
+type APIKeyRetriever func(string, echo.Context) (*store.UserSchema, error)
+
+type APIKeyConfig struct {
+	Skipper   middleware.Skipper
+	Keyname   string
+	Retriever APIKeyRetriever
+}
+
+func APIKeyAuth(config APIKeyConfig) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+
+			value, err := getAPIKey(config.Keyname, c)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+			}
+
+			user, err := config.Retriever(value, c)
+			switch {
+			case err != nil:
+				return err
+			case user == nil:
+				return echo.ErrUnauthorized
+			default:
+				c.Set("User", *user)
+				return next(c)
+			}
+		}
+	}
+}
+
+func keyFromQuery(key string, c echo.Context) (string, error) {
+	value := c.QueryParam(key)
+	if value == "" {
+		return "", fmt.Errorf("no apikey '%s' as query param", key)
+	}
+	return value, nil
+}
+
+func keyFromHeader(key string, c echo.Context) (string, error) {
+	value := c.Request().Header.Get(key)
+	if value == "" {
+		return "", fmt.Errorf("no apikey '%s' as header", key)
+	}
+	return value, nil
+}
+
+func getAPIKey(key string, c echo.Context) (string, error) {
+	if value, err := keyFromHeader(key, c); err == nil {
+		return value, nil
+	}
+
+	if value, err := keyFromQuery(key, c); err == nil {
+		return value, nil
+	}
+
+	return "", fmt.Errorf("no apikey '%s' as query param or header")
+}
