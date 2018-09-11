2c4eeb0d670cdd250c9e47c49cb538024f83f8fa
gtarcea <glenn.tarcea@gmail.com>
2018-09-11 10:46:35 -0400

    Setup storage engine test so that it will loop through a list of storage engines

--- internal/store/users_store_engine_test.go
+++ internal/store/users_store_engine_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestUsersStoreEngine_AddUser(t *testing.T) {
-	e := store.NewUsersStoreEngineMemory()
 	tests := []struct {
 		user       store.UserSchema
 		shouldFail bool
@@ -19,15 +18,18 @@ func TestUsersStoreEngine_AddUser(t *testing.T) {
 		{user: store.UserSchema{ID: "tuser@test.com"}, shouldFail: true, name: "Existing user"},
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			_, err := e.AddUser(test.user)
-			if !test.shouldFail {
-				assert.Okf(t, err, "Failed to add user %s", test.user.ID)
-			} else {
-				assert.Errorf(t, err, "Added existing user %s", test.user.ID)
-			}
-		})
+	engines := createUsersStoreEngines()
+	for _, e := range engines {
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				_, err := e.AddUser(test.user)
+				if !test.shouldFail {
+					assert.Okf(t, err, "Failed to add user %s", test.user.ID)
+				} else {
+					assert.Errorf(t, err, "Added existing user %s", test.user.ID)
+				}
+			})
+		}
 	}
 }
 
@@ -41,19 +43,21 @@ func TestUsersStoreEngine_GetUserByID(t *testing.T) {
 		{id: "nosuchuser@doesnot.exist", shouldFail: true, name: "Fail to find a non-existing user"},
 	}
 
-	e := store.NewUsersStoreEngineMemory()
-	addDefaultUsersToStoreEngine(t, e)
+	engines := createUsersStoreEngines()
+	for _, e := range engines {
+		addDefaultUsersToStoreEngine(t, e)
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			user, err := e.GetUserByID(test.id)
-			if !test.shouldFail {
-				assert.Okf(t, err, "Should have found user %s, error %s", test.id, err)
-				assert.Truef(t, user.ID == test.id, "User and retrieved user not equal %s != %s", user.ID, test.id)
-			} else {
-				assert.Errorf(t, err, "Should have failed retrieving user %s", test.id)
-			}
-		})
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				user, err := e.GetUserByID(test.id)
+				if !test.shouldFail {
+					assert.Okf(t, err, "Should have found user %s, error %s", test.id, err)
+					assert.Truef(t, user.ID == test.id, "User and retrieved user not equal %s != %s", user.ID, test.id)
+				} else {
+					assert.Errorf(t, err, "Should have failed retrieving user %s", test.id)
+				}
+			})
+		}
 	}
 }
 
@@ -68,22 +72,22 @@ func TestUsersStoreEngine_GetUserByAPIKey(t *testing.T) {
 		{id: "nosuchuser@doesnot.exist", apikey: "no such key", shouldFail: true, name: "Lookup apikey that doesn't exist"},
 	}
 
-	e := store.NewUsersStoreEngineMemory()
-	addDefaultUsersToStoreEngine(t, e)
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			user, err := e.GetUserByAPIKey(test.apikey)
-			if !test.shouldFail {
-				assert.Okf(t, err, "Failed to look up existing apikey %s", test.apikey)
-				assert.Truef(t, user.APIKey == test.apikey, "APIKeys don't match for user/expected %s/%s", user.APIKey, test.apikey)
-				assert.Truef(t, user.ID == test.id, "User IDs don't match for user/expected %s/%s", user.ID, test.id)
-			} else {
-				assert.Errorf(t, err, "Found apikey that should not exist %s, user %s", test.apikey, user.ID)
-			}
-		})
+	engines := createUsersStoreEngines()
+	for _, e := range engines {
+		addDefaultUsersToStoreEngine(t, e)
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				user, err := e.GetUserByAPIKey(test.apikey)
+				if !test.shouldFail {
+					assert.Okf(t, err, "Failed to look up existing apikey %s", test.apikey)
+					assert.Truef(t, user.APIKey == test.apikey, "APIKeys don't match for user/expected %s/%s", user.APIKey, test.apikey)
+					assert.Truef(t, user.ID == test.id, "User IDs don't match for user/expected %s/%s", user.ID, test.id)
+				} else {
+					assert.Errorf(t, err, "Found apikey that should not exist %s, user %s", test.apikey, user.ID)
+				}
+			})
+		}
 	}
-
 }
 
 func TestUsersStoreEngine_ModifyUserFullname(t *testing.T) {
@@ -97,19 +101,20 @@ func TestUsersStoreEngine_ModifyUserFullname(t *testing.T) {
 		{id: "doesnot@exist.com", newFullname: "nosuch-changed", shouldFail: true, name: "Set fullname for non-existing user"},
 	}
 
-	e := store.NewUsersStoreEngineMemory()
-	addDefaultUsersToStoreEngine(t, e)
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			user, err := e.ModifyUserFullname(test.id, test.newFullname, time.Now())
-			if !test.shouldFail {
-				assert.Okf(t, err, "Failed to modify existing user fullname id %s", test.id)
-				assert.Truef(t, user.Fullname == test.newFullname, "Expected fullname to equal %s, instead got %s", test.newFullname, user.Fullname)
-			} else {
-				assert.Errorf(t, err, "Attempt to modify user (%s) who does not exist succeeded", test.id)
-			}
-		})
+	engines := createUsersStoreEngines()
+	for _, e := range engines {
+		addDefaultUsersToStoreEngine(t, e)
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				user, err := e.ModifyUserFullname(test.id, test.newFullname, time.Now())
+				if !test.shouldFail {
+					assert.Okf(t, err, "Failed to modify existing user fullname id %s", test.id)
+					assert.Truef(t, user.Fullname == test.newFullname, "Expected fullname to equal %s, instead got %s", test.newFullname, user.Fullname)
+				} else {
+					assert.Errorf(t, err, "Attempt to modify user (%s) who does not exist succeeded", test.id)
+				}
+			})
+		}
 	}
 }
 
@@ -124,19 +129,20 @@ func TestUsersStoreEngine_ModifyUserPassword(t *testing.T) {
 		{id: "doesnot@exist.com", newPassword: "nosuch-changed", shouldFail: true, name: "Set fullname for non-existing user"},
 	}
 
-	e := store.NewUsersStoreEngineMemory()
-	addDefaultUsersToStoreEngine(t, e)
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			user, err := e.ModifyUserPassword(test.id, test.newPassword, time.Now())
-			if !test.shouldFail {
-				assert.Okf(t, err, "Failed to modify existing user password id %s", test.id)
-				assert.Truef(t, user.Password == test.newPassword, "Expected password to equal %s, instead got %s", test.newPassword, user.Password)
-			} else {
-				assert.Errorf(t, err, "Attempt to modify user (%s) who does not exist succeeded", test.id)
-			}
-		})
+	engines := createUsersStoreEngines()
+	for _, e := range engines {
+		addDefaultUsersToStoreEngine(t, e)
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				user, err := e.ModifyUserPassword(test.id, test.newPassword, time.Now())
+				if !test.shouldFail {
+					assert.Okf(t, err, "Failed to modify existing user password id %s", test.id)
+					assert.Truef(t, user.Password == test.newPassword, "Expected password to equal %s, instead got %s", test.newPassword, user.Password)
+				} else {
+					assert.Errorf(t, err, "Attempt to modify user (%s) who does not exist succeeded", test.id)
+				}
+			})
+		}
 	}
 }
 
@@ -151,19 +157,20 @@ func TestUsersStoreEngine_ModifyUserAPIKey(t *testing.T) {
 		{id: "doesnot@exist.com", newAPIKey: "nosuch-changed", shouldFail: true, name: "Set fullname for non-existing user"},
 	}
 
-	e := store.NewUsersStoreEngineMemory()
-	addDefaultUsersToStoreEngine(t, e)
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			user, err := e.ModifyUserAPIKey(test.id, test.newAPIKey, time.Now())
-			if !test.shouldFail {
-				assert.Okf(t, err, "Failed to modify existing user APIKey id %s", test.id)
-				assert.Truef(t, user.APIKey == test.newAPIKey, "Expected APIKey to equal %s, instead got %s", test.newAPIKey, user.APIKey)
-			} else {
-				assert.Errorf(t, err, "Attempt to modify user (%s) who does not exist succeeded", test.id)
-			}
-		})
+	engines := createUsersStoreEngines()
+	for _, e := range engines {
+		addDefaultUsersToStoreEngine(t, e)
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				user, err := e.ModifyUserAPIKey(test.id, test.newAPIKey, time.Now())
+				if !test.shouldFail {
+					assert.Okf(t, err, "Failed to modify existing user APIKey id %s", test.id)
+					assert.Truef(t, user.APIKey == test.newAPIKey, "Expected APIKey to equal %s, instead got %s", test.newAPIKey, user.APIKey)
+				} else {
+					assert.Errorf(t, err, "Attempt to modify user (%s) who does not exist succeeded", test.id)
+				}
+			})
+		}
 	}
 }
 
@@ -176,4 +183,10 @@ func addDefaultUsersToStoreEngine(t *testing.T, e store.UsersStoreEngine) {
 		_, err := e.AddUser(user)
 		assert.Okf(t, err, "Failed to add user %s", user.ID)
 	}
+}
+
+func createUsersStoreEngines() []store.UsersStoreEngine {
+	var engines []store.UsersStoreEngine
+	engines = append(engines, store.NewUsersStoreEngineMemory())
+	return engines
 }
