bd75f072ced91ccefae6135830d5cbb35305ac9f
gtarcea <glenn.tarcea@gmail.com>
2018-09-12 14:39:17 -0400

    Extend ProjectExtendedModel to add in a ProjectUserModel and a FilesCount field

--- internal/store/project.go
+++ internal/store/project.go
@@ -13,6 +13,8 @@ type ProjectSchema struct {
 
 type ProjectExtendedModel struct {
 	ProjectSchema
+	FilesCount    int                  `json:"files_count"`
+	Users         []ProjectUserModel   `json:"users"`
 	Samples       []SampleSchema       `json:"samples"`
 	Processes     []ProcessSchema      `json:"processes"`
 	Experiments   []ExperimentSchema   `json:"experiments"`
@@ -22,4 +24,14 @@ type ProjectExtendedModel struct {
 type ProjectRelationships struct {
 	Process2Sample    []Process2Sample    `json:"process2sample"`
 	Experiment2Sample []Experiment2Sample `json:"experiment2sample"`
+}
+
+type ProjectUserModel struct {
+	ID        string    `db:"id" json:"id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	BetaUser  bool      `db:"beta_user" json:"beta_user"`
+	Fullname  string    `db:"fullname" json:"fullname"`
+	ProjectID string    `db:"project_id" json:"project_id"`
+	UserID    string    `db:"user_id" json:"user_id"`
 }
