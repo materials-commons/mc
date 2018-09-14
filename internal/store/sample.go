bc5271d2b56c0a8dadfe5b30dc8090933628da6f
gtarcea <glenn.tarcea@gmail.com>
2018-09-14 17:00:54 -0400

    Use Model embedded struct

--- internal/store/sample.go
+++ internal/store/sample.go
@@ -1,13 +1,6 @@
 package store
 
-import "time"
-
 type SampleSchema struct {
-	ID          string    `db:"id" json:"id"`
-	CreatedAt   time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
-	Name        string    `db:"name" json:"name"`
-	Description string    `db:"description" json:"description"`
-	OType       string    `db:"otype" json:"otype"`
-	Owner       string    `db:"owner" json:"owner"`
+	Model
+	Description string `db:"description" json:"description"`
 }
