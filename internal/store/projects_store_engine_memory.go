092ab293fc1a9defb7d4562a0ed1d52c605241b6
gtarcea <glenn.tarcea@gmail.com>
2018-10-17 13:20:52 -0400

    Implement in memory version of projects store engine

--- internal/store/projects_store_engine_memory.go
+++ internal/store/projects_store_engine_memory.go
@@ -1,6 +1,10 @@
 package store
 
-import "time"
+import (
+	"time"
+
+	"github.com/hashicorp/go-uuid"
+)
 
 type ProjectsStoreEngineMemory struct {
 	DB map[string]ProjectSchema
@@ -19,29 +23,85 @@ func NewProjectsStoreEngineMemoryWithDB(db map[string]ProjectSchema) *ProjectsSt
 }
 
 func (e *ProjectsStoreEngineMemory) AddProject(project ProjectSchema) (ProjectSchema, error) {
-	return ProjectSchema{}, nil
+	var err error
+	if project.ID, err = uuid.GenerateUUID(); err != nil {
+		return ProjectSchema{}, err
+	}
+
+	e.DB[project.ID] = project
+
+	return project, nil
 }
 
 func (e *ProjectsStoreEngineMemory) GetProject(id string) (ProjectExtendedModel, error) {
-	return ProjectExtendedModel{}, nil
+	proj, ok := e.DB[id]
+	if !ok {
+		return ProjectExtendedModel{}, ErrNotFound
+	}
+
+	p := ProjectExtendedModel{
+		ProjectSchema: proj,
+	}
+	return p, nil
 }
 
 func (e *ProjectsStoreEngineMemory) GetProjectSimple(id string) (ProjectSimpleModel, error) {
-	return ProjectSimpleModel{}, nil
+	proj, ok := e.DB[id]
+	if !ok {
+		return ProjectSimpleModel{}, ErrNotFound
+	}
+
+	p := ProjectSimpleModel{
+		ProjectSchema: proj,
+	}
+
+	return p, nil
 }
 
 func (e *ProjectsStoreEngineMemory) GetAllProjectsForUser(user string) ([]ProjectExtendedModel, error) {
-	return nil, nil
+	var userProjects []ProjectExtendedModel
+	for _, proj := range e.DB {
+		if proj.Owner == user {
+			p := ProjectExtendedModel{
+				ProjectSchema: proj,
+			}
+			userProjects = append(userProjects, p)
+		}
+	}
+	return userProjects, nil
 }
 
 func (e *ProjectsStoreEngineMemory) DeleteProject(id string) error {
+	_, ok := e.DB[id]
+	if !ok {
+		return ErrNotFound
+	}
+
+	delete(e.DB, id)
+
 	return nil
 }
 
 func (e *ProjectsStoreEngineMemory) UpdateProjectName(id string, name string, updatedAt time.Time) error {
+	proj, ok := e.DB[id]
+	if !ok {
+		return ErrNotFound
+	}
+
+	proj.Name = name
+	proj.MTime = updatedAt
+	e.DB[id] = proj
 	return nil
 }
 
 func (e *ProjectsStoreEngineMemory) UpdateProjectDescription(id string, description string, updatedAt time.Time) error {
+	proj, ok := e.DB[id]
+	if !ok {
+		return ErrNotFound
+	}
+
+	proj.Description = description
+	proj.MTime = updatedAt
+	e.DB[id] = proj
 	return nil
 }
