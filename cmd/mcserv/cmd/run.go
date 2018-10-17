413c3105b4848041a627e31c62c454ac7176c131
gtarcea <glenn.tarcea@gmail.com>
2018-10-17 11:27:35 -0400

    Update help

--- cmd/mcserv/cmd/run.go
+++ cmd/mcserv/cmd/run.go
@@ -1,17 +1,3 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -31,14 +17,9 @@ import (
 // siteCmd represents the site command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: cliCmdRun,
+	Short: "Runs the mcserv file processor and API",
+	Long:  `mcserv provides and API and background processor for loading files into Materials Commons that were uploaded into a given directory.`,
+	Run:   cliCmdRun,
 }
 
 var (
