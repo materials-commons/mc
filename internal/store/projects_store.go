22620fafae6aedcc5bd7d44a8e392777a3ab592c
gtarcea <glenn.tarcea@gmail.com>
2018-10-18 16:45:42 -0400

    Implement AddProject for projects store

--- internal/store/projects_store.go
+++ internal/store/projects_store.go
@@ -1,11 +1,34 @@
 package store
 
+import "time"
+
 type ProjectsStore struct {
 	pStoreEngine ProjectsStoreEngine
 }
 
 func NewProjectsStore(e ProjectsStoreEngine) *ProjectsStore {
 	return &ProjectsStore{pStoreEngine: e}
+}
+
+func (s *ProjectsStore) AddProject(pModel AddProjectModel) (ProjectSchema, error) {
+	if err := pModel.Validate(); err != nil {
+		return ProjectSchema{}, err
+	}
+
+	now := time.Now()
+
+	p := ProjectSchema{
+		Model: Model{
+			Name:      pModel.Name,
+			Owner:     pModel.Owner,
+			Birthtime: now,
+			MTime:     now,
+			OType:     "project",
+		},
+		Description: pModel.Description,
+	}
+
+	return s.pStoreEngine.AddProject(p)
 }
 
 func (s *ProjectsStore) GetProjectSimple(id string) (ProjectSimpleModel, error) {
