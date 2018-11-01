220ce7da76cb8e43ca0830e06025b43f311c4f3a
gtarcea <glenn.tarcea@gmail.com>
2018-11-01 15:25:07 -0400

    Remove viper setup

--- cmd/mcserv/cmd/root.go
+++ cmd/mcserv/cmd/root.go
@@ -32,11 +32,8 @@ import (
 	"github.com/materials-commons/mc/internal/file"
 	"github.com/materials-commons/mc/internal/store"
 
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-
 	m "github.com/materials-commons/mc/internal/middleware"
+	"github.com/spf13/cobra"
 
 	r "gopkg.in/gorethink/gorethink.v4"
 )
@@ -78,8 +75,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mcserv.yaml)")
 	rootCmd.Flags().IntVarP(&port, "port", "p", 4000, "Port to listen on")
 	rootCmd.Flags().IntVarP(&numberOfWorkers, "workers", "w", 10, "Number of workers to use for processing uploads")
@@ -109,32 +104,6 @@ func cliCmdRoot(cmd *cobra.Command, args []string) {
 	backgroundLoader.Start(ctx)
 
 	e.Start(fmt.Sprintf(":%d", port))
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".mcserv" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".mcserv")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
 
 func connectToDB() store.DB {
